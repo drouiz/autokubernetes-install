f136c680db5cce5e2cc5bf7cd26963cc329d2b70
Drouiz <info@nunovalencia.info>
2020-01-04 00:50:58 +0100

    fist commit

---
+++ install-system.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#apt-add-repository --yes --update ppa:ansible/ansibl
+#apt-get update -y
+#apt-get upgrade -y
+#apt install virtualbox
+
+#curl -O https://releases.hashicorp.com/vagrant/2.2.6/vagrant_2.2.6_x86_64.deb
+
+#apt install ./vagrant_2.2.6_x86_64.deb -y
+#apt install software-properties-common -y
+#apt install ansible -y
+
+vagrant destroy -f
+vagrant up
